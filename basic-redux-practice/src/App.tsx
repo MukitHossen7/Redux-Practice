4b9e903ced023ac9a5bc781b324c958f5342585c
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-25 11:43:48 +0600

    refactor: remove unused CSS and assets, update App component layout

--- basic-redux-practice/src/App.tsx
+++ basic-redux-practice/src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex gap-10 items-center mx-auto w-96 py-20">
+      <button className="px-4 py-2 border-2 border-green-500 rounded-xl">
+        Increment
+      </button>
+      <div className="text-2xl text-red-500 font-bold">0</div>
+      <button className="px-4 py-2 border-2 border-green-500 rounded-xl">
+        Decrement
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
