0bf4f50c79ebe5821b39e2276ec02975b90934bb
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-25 15:41:39 +0600

    feat: implement increment and decrement functionality in counter slice and update App component

--- basic-redux-project/src/App.tsx
+++ basic-redux-project/src/App.tsx
@@ -1,11 +1,27 @@
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+
 function App() {
+  const dispatch = useDispatch();
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
   return (
     <div className="flex gap-10 items-center mx-auto w-96 py-20">
-      <button className="px-4 py-2 border-2 border-green-500 rounded-xl">
+      <button
+        onClick={handleIncrement}
+        className="px-4 py-2 border-2 border-green-500 rounded-xl"
+      >
         Increment
       </button>
       <div className="text-2xl text-red-500 font-bold">0</div>
-      <button className="px-4 py-2 border-2 border-green-500 rounded-xl">
+      <button
+        onClick={handleDecrement}
+        className="px-4 py-2 border-2 border-green-500 rounded-xl"
+      >
         Decrement
       </button>
     </div>
