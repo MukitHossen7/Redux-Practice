d8c1127de85cc01f2321f3e0b4c72f56d929e10d
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-27 01:14:59 +0600

    feat: update App component layout and add button for interaction

--- todo-app-redux/src/App.tsx
+++ todo-app-redux/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
-    <div>
+    <div className="flex text-center justify-center gap-10 py-14">
       <h2>Hello</h2>
+      <Button>Say Hi</Button>
     </div>
   );
 }
