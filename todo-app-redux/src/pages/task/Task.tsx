bcbefb9b73b502422e123cac7d67ab5a29a08556
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-29 01:05:03 +0600

    feat: implement task filtering functionality in Tabs component

--- todo-app-redux/src/pages/task/Task.tsx
+++ todo-app-redux/src/pages/task/Task.tsx
@@ -2,20 +2,41 @@ import TaskCard from "@/components/task/TaskCard";
 import { TaskModel } from "@/components/task/TaskModel";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import type { ITasks } from "@/interface/tasks.interface/tasks.interface";
-import { selectTasks } from "@/redux/features/task/taskSlice";
-import { useAppSelector } from "@/redux/hook";
+import { filterTask, selectTasks } from "@/redux/features/task/taskSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
 
 const Task = () => {
   const tasks = useAppSelector(selectTasks);
+  const dispatch = useAppDispatch();
   return (
     <div className="py-10">
       <div className="flex max-w-2xl mx-auto justify-between">
         <Tabs defaultValue="All">
           <TabsList className="grid grid-cols-4">
-            <TabsTrigger value="All">All</TabsTrigger>
-            <TabsTrigger value="Low">Low</TabsTrigger>
-            <TabsTrigger value="Medium">Medium</TabsTrigger>
-            <TabsTrigger value="High">High</TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(filterTask("All"))}
+              value="All"
+            >
+              All
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(filterTask("Low"))}
+              value="Low"
+            >
+              Low
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(filterTask("Medium"))}
+              value="Medium"
+            >
+              Medium
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(filterTask("High"))}
+              value="High"
+            >
+              High
+            </TabsTrigger>
           </TabsList>
         </Tabs>
         <TaskModel></TaskModel>
