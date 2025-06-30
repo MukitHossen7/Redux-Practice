2d4bcb184faf9c3b5cf1e58157098b2a942edc07
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-30 16:47:53 +0600

    feat: add quiz API with getQuiz endpoint using Redux Toolkit Query

---
+++ jakanaka-quiz/src/redux/api/quiz.api/quizApi.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const quizApi = createApi({
+  reducerPath: "quizApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  tagTypes: ["quiz"],
+  endpoints: (builder) => ({
+    getQuiz: builder.query({
+      query: () => "/quizzes",
+      providesTags: ["quiz"],
+    }),
+  }),
+});
+
+export const { useGetQuizQuery } = quizApi;
