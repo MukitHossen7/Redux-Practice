419d4d7687ed6316e088c8238cb756db8297bebd
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-01 16:18:15 +0600

    feat: add footer, navbar, search, video grid, pagination, and tags components with Tailwind CSS styling

---
+++ think-in-a-redux-way/src/components/search/Search.js
@@ -0,0 +1,23 @@
+const Search = () => {
+  return (
+    <>
+      <div class="border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200">
+        <form>
+          <input
+            class="outline-none border-none mr-2"
+            type="search"
+            name="search"
+            placeholder="Search"
+          />
+        </form>
+        <img
+          class="inline h-4 cursor-pointer"
+          src="./assets/search.svg"
+          alt="Search"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Search;
---
+++ think-in-a-redux-way/src/components/tags/Tag.js
@@ -0,0 +1,15 @@
+const Tag = () => {
+  return (
+    <div class="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
+      react
+    </div>
+  );
+};
+
+export default Tag;
+
+{
+  /* <div class="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer">
+            redux
+          </div> */
+}
