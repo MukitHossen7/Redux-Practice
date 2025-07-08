3911fce5b126c43de563032a64b741b7315fea23
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-08 16:23:32 +0600

    feat: implement video fetching logic in VideoGrid component and handle loading and error states

--- think-in-a-redux-way/src/components/grid/VideoGrid.js
+++ think-in-a-redux-way/src/components/grid/VideoGrid.js
@@ -1,13 +1,33 @@
 import VideoGridItem from "./VideoGridItem";
-
+import { useDispatch, useSelector } from "react-redux";
+import { fetchVideos } from "./../../features/videos/videosSlice";
+import { useEffect } from "react";
 const VideoGrid = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchVideos());
+  }, [dispatch]);
+  const { videos, isLoading, isError, error } = useSelector(
+    (state) => state.video
+  );
+  console.log(videos);
+  let content;
+  if (isLoading) content = <div className="col-span-12">Loading....</div>;
+  if (!isLoading && isError)
+    content = <div className="col-span-12">{error}</div>;
+  if (!isError && !isLoading && videos?.length === 0)
+    content = <div className="col-span-12">Video not Found</div>;
+  if (!isError && !isLoading && videos?.length > 0)
+    content = videos.map((item) => (
+      <VideoGridItem key={item.id}></VideoGridItem>
+    ));
   return (
     <div>
       <section className="pt-12">
         <section className="pt-12">
           <div className="grid grid-cols-4 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
-            <VideoGridItem></VideoGridItem>
-            {/* <div className="col-span-12">some error happened</div> */}
+            {content}
           </div>
         </section>
       </section>
