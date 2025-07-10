d0fa5c260e8921204ad749483fcbe870aa2e6707
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-10 15:45:44 +0600

    create tag slice for get all tags

--- think-in-a-redux-way/src/components/tags/Tag.js
+++ think-in-a-redux-way/src/components/tags/Tag.js
@@ -1,15 +1,9 @@
-const Tag = () => {
+const Tag = ({ tag }) => {
   return (
     <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
-      react
+      {tag.title}
     </div>
   );
 };
 
 export default Tag;
-
-{
-  /* <div className="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer">
-            redux
-          </div> */
-}
--- think-in-a-redux-way/src/components/tags/Tags.js
+++ think-in-a-redux-way/src/components/tags/Tags.js
@@ -1,15 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
 import Tag from "./Tag";
+import { useEffect } from "react";
+import { fetchTags } from "../../features/tags/tagsSlice";
 
 const Tags = () => {
-  return (
-    <div>
-      <section>
-        <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
-          <Tag></Tag>
-        </div>
-      </section>
-    </div>
-  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTags());
+  }, [dispatch]);
+  const { tags } = useSelector((state) => state.tag);
+
+  return tags.length > 0 ? (
+    <section>
+      <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
+        {tags.map((tag) => (
+          <Tag key={tag.id} tag={tag}></Tag>
+        ))}
+      </div>
+    </section>
+  ) : null;
 };
 
 export default Tags;
